9e0c3b6c636741196106fcffd401c09892aa9b3c
Marlom Klismann <MKlismann@users.noreply.github.com>
2020-08-06 14:22:40 -0300

    Inclusão da possibilidade de mais de uma tentativa de palpite + Refatorações.

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,28 +1,31 @@
 print("******************************************************************")
 print("*            Bem vindo ao jogo de ---ADIVINHAÇÕES--- !!!         *")
 print("******************************************************************")
-print("")
 
-numero_secreto = 42
+numero_secreto      = 42
+total_de_tentativas = 3
+rodada              = 1
 
-chute = input("Digite um número (palpite): ")
-chute = int(chute)
+while (rodada <= total_de_tentativas):
+    print("\nTentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = input("Digite um número (palpite): ")
+    chute = int(chute)
+    print("\nVocê digitou: {}".format(chute))
 
-print("")
-print("Você digitou:", chute, sep=" ")
+    palpite_correto = chute == numero_secreto
+    palpite_maior   = chute > numero_secreto
+    palpite_menor   = chute < numero_secreto
 
-palpite_correto = chute == numero_secreto
-palpite_maior = chute > numero_secreto
-palpite_menor = chute < numero_secreto
+    if(palpite_correto):
+        print("Parabéns, você acertou! \o/")
+    else:
+        if(palpite_maior):
+            print("Que pena, você errou! :(  O seu palpite foi maior do que o número secreto.")
+        elif(palpite_menor):
+            print("Que pena, você errou! :(  O seu palpite foi menor do que o número secreto.")
 
-if(palpite_correto):
-    print("Parabéns, você acertou! \o/")
-else:
-    if(palpite_maior):
-        print("Que pena, você errou! :(  O seu palpite foi maior do que o número secreto.")
-    elif(palpite_menor):
-        print("Que pena, você errou! :(  O seu palpite foi menor do que o número secreto.")
-    print("O número correto era:", numero_secreto)
+    rodada = rodada + 1
 
-print("")
-print("Fim do jogo.")
+print("\n******************************************************************")
+print("*                        Fim do jogo !!!                         *")
+print("******************************************************************")
