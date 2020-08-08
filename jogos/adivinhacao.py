487718837b83671bfbf23b248730fa4f75a3b8a3
Marlom Klismann <MKlismann@users.noreply.github.com>
2020-08-07 21:01:31 -0300

    Definição de níveis de dificuldade + Implementação de pontuação no jogo

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -8,14 +8,26 @@ print("******************************************************************")
 # Geração do número aleatório; a função random() gera números entre 1 e 101-1.
 numero_secreto = round(random.randrange(1, 101))
 
-# Quantidade máxima de tentativas/palpites no jogo.
-total_de_tentativas = 3
+# Pontuação inicial do jogador.
+pontos = 1000
+
+# Define a quantidade máxima de tentativas/palpites no jogo, de acordo com o nível escolhido pelo jogador.
+nivel = input("\nEscolha o nível de dificuldade do jogo \n1 - Fácil \n2 - Médio \n3 - Difícil \n-> Nível selecionado: ")
+nivel = int(nivel)
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+print(f"\nSua pontuação inicial é de {pontos} pontos.")
 
 for rodada in range(1, total_de_tentativas + 1):
-    print(f"\nTentativa {rodada} de {total_de_tentativas}")
-    chute = input("Digite um número (palpite) entre 1 e 100: ")
+    print(f"\nTentativa {rodada} de {total_de_tentativas}.")
+    chute = input("-> Digite um número (palpite) entre 1 e 100: ")
     chute = int(chute)
-    print(f"\nVocê digitou: {chute}")
+    print(f"\nVocê digitou: {chute}.")
 
     if chute < 1 or chute > 100:
         print("Você deve digitar um número entre 1 e 100!!!")
@@ -27,12 +39,19 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if palpite_correto:
         print("Parabéns, você acertou! :)")
+        print(f"Sua pontuação final foi de {pontos} pontos.")
         break
     else:
         if palpite_maior:
             print("Que pena, você errou! :(  O seu palpite foi maior do que o número secreto.")
         elif palpite_menor:
             print("Que pena, você errou! :(  O seu palpite foi menor do que o número secreto.")
+
+        # Cada palpite errado causa uma perda de pontos de acordo com a seguinte regra:
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
+        print(f"Sua pontuação sofreu uma penalidade de -{pontos_perdidos} pontos.")
+        print(f"Sua pontuação atual é de {pontos} pontos.")
 
     # Se o jogador errar todas as tentativas/palpites, informa qual era o número secreto;
     if rodada == total_de_tentativas and not palpite_correto:
